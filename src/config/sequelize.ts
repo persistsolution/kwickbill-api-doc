2b5e370bc418355255a6fe43554488f5798b42a5
persistsolution <persistsolution1@gmail.com>
2025-02-13 12:37:30 +0530

    Update sequelize.ts

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("kwickbill_stage", "postgres", "postgres12345", {
-  host: "database-2.crq6yqcualdr.ap-south-1.rds.amazonaws.com",
+export const sequelize = new Sequelize("kwickbill_dev", "postgres", "Rd9595454907", {
+  host: "database-4.crq6yqcualdr.ap-south-1.rds.amazonaws.com",
   port: 5432,
   dialect: "postgres", // Or 'mysql' | 'mssql' | 'sqlite'
   dialectOptions: {
