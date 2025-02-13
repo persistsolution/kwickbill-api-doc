60371c42aada6706eca946cec7ad37b59a942dea
persistsolution-admin <persistsolution1@gmail.com>
2025-02-13 14:26:22 +0530

    update

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-<<<<<<< HEAD
 export const sequelize = new Sequelize(
   process.env.DEV_DB_NAME || "kwickbill",
   process.env.DEV_DB_USER || "postgres",
@@ -16,14 +15,3 @@ export const sequelize = new Sequelize(
     logging: process.env.DB_LOGGING === "true", // Enable logging if set to "true"
   }
 );
-=======
-export const sequelize = new Sequelize("kwickbill_dev", "postgres", "Rd9595454907", {
-  host: "database-4.crq6yqcualdr.ap-south-1.rds.amazonaws.com",
-  port: 5432,
-  dialect: "postgres", // Or 'mysql' | 'mssql' | 'sqlite'
-  dialectOptions: {
-    ssl: false, // Disable SSL explicitly
-  },
-  logging: false, // Optional: Disable logging
-});
->>>>>>> dd27a0ed3b2c2fc6239c427d36db4e8518499fde
