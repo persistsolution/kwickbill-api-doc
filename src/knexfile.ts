9664a65e25903bbca298e11180ae3bc581ec90c2
persistsolution <persistsolution1@gmail.com>
2025-02-13 12:48:34 +0530

    Update knexfile.ts

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,10 +4,10 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg', // PostgreSQL client
     connection: {
-      host: 'database-2.crq6yqcualdr.ap-south-1.rds.amazonaws.com',
+      host: 'database-4.crq6yqcualdr.ap-south-1.rds.amazonaws.com',
       user: 'postgres',
-      password: 'postgres12345',
-      database: 'kwickbill_stage',
+      password: 'Rd9595454907',
+      database: 'kwickbill_dev',
     },
     migrations: {
       directory: './migrations', // Path to migration files
